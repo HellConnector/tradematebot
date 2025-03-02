08893983a0682ad136b8983184f6a0fabba056b5
HellConnector <50717671+HellConnector@users.noreply.github.com>
2025-03-03 00:44:40 +0100

    Update main.py

--- price-worker/main.py
+++ price-worker/main.py
@@ -282,12 +282,12 @@ async def main():
         stop = time.monotonic()
         logger.info(f"Iteration completed in {stop-start:.2f}s")
         manager.show_progress()
-    logger.info(f"----------------------------SUMMARY----------------------------")
+    logger.info(f"---------------------------SUMMARY---------------------------")
     logger.info(f"Has price -> {manager.success_count} items")
     logger.info(f"Doesn't have price -> {manager.remaining_count} items")
     for item in filter(lambda i: i.success_no_price, manager.items.values()):
         logger.info(item)
-    logger.info(f"----------------------------SUMMARY----------------------------")
+    logger.info(f"---------------------------SUMMARY---------------------------")
 
 
 def run():
